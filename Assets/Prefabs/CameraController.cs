d57ad36da866acf536e1a3568a3050f702217d76
kohrabi <63339885+kohrabi@users.noreply.github.com>
2023-08-12 23:11:12 +0700

    Camera Follow Player + Map

---
+++ Assets/Prefabs/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector2 moveDir = Vector2.one;
+    private Vector3 velocity = Vector3.zero;
+    private float smoothTime = 0.25f;
+
+    [SerializeField] private Vector3 CameraOffset = new Vector3(2f, 2f, -10f);
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveDir = GameObject.Find("Player").GetComponent<PlayerScript>().moveDir;
+        Vector3 offset = moveDir;
+        offset.Scale(CameraOffset);
+        offset.z = -10f;
+        Vector3 targetPos =  target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Prefabs/Player/PlayerScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float MovementSpeed = 10f;
+
+    private Rigidbody2D rb;
+    public Vector2 moveDir;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * MovementSpeed, moveDir.y * MovementSpeed);
+    }
+}
