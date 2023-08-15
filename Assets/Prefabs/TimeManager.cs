dc3db09aba4cc0a057a9b7dcca71b716ffe9ae84
kohrabi <63339885+kohrabi@users.noreply.github.com>
2023-08-15 15:31:04 +0700

    Lighting when night and a main menu

--- Assets/Prefabs/TimeManager.cs
+++ Assets/Prefabs/TimeManager.cs
@@ -12,12 +12,18 @@ public class TimeManager : MonoBehaviour
     public float InvertColorThreshold = 0.7f;
 
     public float dayTime = 0;
+
+    GameObject cam;
+    GameObject lightCam;
+
     // Start is called before the first frame update
     void Start()
     {
         spawner = GameObject.FindWithTag("EnemySpawner").GetComponent<EnemySpawner>();
         InvertColor.SetFloat("_Threshold", InvertColorThreshold);
         dayTime = Time.time + DayTime;
+        cam = GameObject.FindWithTag("MainCamera");
+        lightCam = cam.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -33,6 +39,8 @@ public class TimeManager : MonoBehaviour
             {
                 dayTime = Time.time + dayTime;
                 spawner.isDay = false;
+                cam.GetComponent<LightingCamera>().enabled = false;
+                lightCam.SetActive(false);
             }
         }
         // if out of daytime && is not night time
@@ -41,7 +49,11 @@ public class TimeManager : MonoBehaviour
             // switch to night time
             InvertColor.SetFloat("_Threshold", Mathf.Lerp(threshold, 0, DayNightChangeSpeed));
             if (threshold <= DayNightChangeSpeed + DayNightChangeSpeed / 10f)
+            {
                 spawner.SwitchNightTime();
+                cam.GetComponent<LightingCamera>().enabled = true;
+                lightCam.SetActive(true);
+            }
         }
     }
 }
---
+++ Assets/SwordButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SwordButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    GameObject arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrow = GameObject.Find("MainMenu").transform.GetChild(2).gameObject;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        arrow.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        arrow.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
